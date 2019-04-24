eb58f0c97a5602b2fba8c0869ed25a7ed8afeec7
youtoubr <49153072+youtoubr@users.noreply.github.com>
2019-04-24 20:13:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["406039166959353857"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
